70b1e120deeb2eda376e1c55d7137f61c00ff88b
numRandom <56953303+numRandom@users.noreply.github.com>
2019-11-14 22:55:58 +0800

    Add files via upload
    在一个数组中，每隔两个删除一个数，若在数组末，从数组头重新开始。例如，输入8，0->1->2(删除)->3->4->5(删除)->6->7->0(删除)……

---
+++ SubSecondNum.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SubSecondNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            int n = sc.nextInt();
+            List<Integer> list = new LinkedList<Integer>();
+            for (int i = 0; i < n; i++) {
+                list.add(i);
+            }
+            int i = 0;
+            while (list.size() > 1) {
+                i = (i + 2) % list.size();
+                list.remove(i);
+            }
+            System.out.println(list.get(0));
+        }
+    }
+}
